7de00456c4de179766aebea765a2a3d5beaf9764
MariaAddict <mashakan27@gmail.com>
2020-10-11 00:14:22 +0700

    fix: добавить класс api, реализовать подстановку данных пользователя в компоненте Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import profileImage from '../images/profile-image.jpg';
+import api from '../utils/api';
 
+//userName, userDescription , userAvatar
 function Main(props) {
-    return(
+
+    const [userName, setuserName] = React.useState('');
+    const [userDescription, setUserDescription] = React.useState('');
+    const [userAvatar, setUserAvatar] = React.useState('');
+
+    React.useEffect(() => {
+
+
+        api.getUserInfo().then(dataUser => {
+            setuserName(dataUser.name);
+            setUserDescription(dataUser.about);
+            setUserAvatar(dataUser.avatar);
+        }).catch(err => {
+            console.log(err);
+        });;
+    }, []);
+
+    return (
         <main className="content">
             <section className="profile">
-                <div className="profile__overlay" onClick  = {props.onEditAvatar}>
-                  <img src={profileImage} alt="Фото профиля" className="profile__image"/>
+                <div className="profile__overlay" onClick={props.onEditAvatar}>
+                    <img src={userAvatar} alt="Фото профиля" className="profile__image" />
                 </div>
                 <div className="profile__info">
-                    <h1 className="profile__name">Жак-Ив Кусто</h1>
+                    <h1 className="profile__name">{userName}</h1>
                     <button type="button" className="profile__info-button" onClick={props.onEditProfile} ></button>
-                    <p className="profile__activity-type">Исследователь океана</p>
+                    <p className="profile__activity-type">{userDescription}</p>
                 </div>
                 <button type="button" className="profile__add-button" onClick={props.onAddPlace}></button>
             </section>
---
+++ src/utils/api.js
@@ -0,0 +1,131 @@
+class Api {
+    constructor(url, { headers }) {
+        this._url = url;
+        this._headers = headers;
+    }
+
+    getInitialCards() {
+        return fetch(`${this._url}cards`, {
+            method: 'GET',
+            headers: this._headers
+        })
+            .then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+                return Promise.reject(res.status);
+            })
+    }
+
+    addCard(card) {
+        return fetch(`${this._url}cards`, {
+            method: 'POST',
+            headers: this._headers,
+            body: JSON.stringify({
+                name: card.name,
+                link: card.link
+            })
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(res.status);
+        })
+    }
+
+    deleteCard(id) {
+        return fetch(`${this._url}cards/${id}`, {
+            method: 'DELETE',
+            headers: this._headers
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(res.status);
+        });
+    }
+
+    getUserInfo() {
+        return fetch(`${this._url}users/me`, {
+            method: 'GET',
+            headers: this._headers
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(res.status);
+        })
+    }
+
+    editUserInfo(userInfo) {
+        return fetch(`${this._url}users/me`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({
+                name: userInfo.name,
+                about: userInfo.activity
+            })
+        }).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(res.status);
+        });
+    }
+
+    addLike(cardId) {
+        return fetch(`${this._url}cards/likes/${cardId}`, {
+            method: 'PUT',
+            headers: this._headers
+        }
+        ).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(res.status);
+        });
+    }
+
+    deleteLike(cardId) {
+        return fetch(`${this._url}cards/likes/${cardId}`, {
+            method: 'DELETE',
+            headers: this._headers
+        }
+        ).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(res.status);
+        });
+    }
+
+    changeAvatar(input) {
+        return fetch(`${this._url}users/me/avatar`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({
+                avatar: input.link
+            })
+        }
+        ).then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(res.status);
+        });
+    }
+
+    getAppInfo() {
+        return Promise.all([this.getInitialCards(), this.getUserInfo()]);
+    }
+}
+
+//создание api
+const api = new Api('https://mesto.nomoreparties.co/v1/cohort-15/', {
+    headers: {
+        'Content-Type': 'application/json',
+        authorization: 'e7e08b6b-adf3-43f0-9ed1-13df27223916'
+    }
+});
+
+export default api;
