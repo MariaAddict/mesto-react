a23f6c292edad6add4ae020b8bb2b43ca53b04ca
MariaAddict <mashakan27@gmail.com>
2020-10-24 14:36:54 +0700

    refactor: объявить PropTypes для компонента AddPlacePopup

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PopupWithForm from './PopupWithForm';
+import PropTypes from 'prop-types';
 
-function AddPlacePopup({isOpen, onClose, onAddPlace }) {
+function AddPlacePopup({ isOpen, onClose, onAddPlace }) {
     const [name, setName] = React.useState('');
     const [link, setLink] = React.useState('');
 
-    function handleNewName(evt){
+    function handleNewName(evt) {
         setName(evt.target.value);
     }
 
@@ -15,7 +16,7 @@ function AddPlacePopup({isOpen, onClose, onAddPlace }) {
 
     function handleAddPlacePopupSubmit(evt) {
         evt.preventDefault();
-        onAddPlace ({name, link});
+        onAddPlace({ name, link });
         setName('');
         setLink('');
     }
@@ -24,14 +25,20 @@ function AddPlacePopup({isOpen, onClose, onAddPlace }) {
     return (
         <PopupWithForm name='add' title='Новое место' isOpen={isOpen} onClose={onClose} onSubmit={handleAddPlacePopupSubmit} >
             <input type="text" name="name" className="modal__item modal__item_type_header-image" placeholder="Название" id="name"
-             onChange ={handleNewName} value ={name}  />
+                onChange={handleNewName} value={name} />
             <span className="modal__error modal__error_visible" id="name-error"></span>
             <input type="url" name="link" className="modal__item modal__item_type_url-image" placeholder="Ссылка на картинку"
-             id="url" onChange ={handleNewLink} value ={link}  />
+                id="url" onChange={handleNewLink} value={link} />
             <span className="modal__error modal__error_visible" id="url-error"></span>
             <button type="submit" className="modal__save-button"><span className="modal__name-button">Создать</span></button>
         </PopupWithForm>
     );
+}
+
+AddPlacePopup.propTypes = {
+    isOpen: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired,
+    onAddPlace: PropTypes.func.isRequired
 }
 
 export default AddPlacePopup;
